27325a428bc875bca634a2aeb8a60ad216861d74
mywrh <68284076+mywrh@users.noreply.github.com>
2026-04-01 15:35:40 +0800

    #

--- src/components/map/MapContainer.tsx
+++ src/components/map/MapContainer.tsx
@@ -458,7 +458,7 @@ export const MapContainer: React.FC<Props> = ({ setStationMenu, isDraggingRef })
 
         // Calculate dynamic zoom scale
         let scale = Math.pow(2, currentZoom - 10);
-        scale = Math.max(0.5, Math.min(scale, 3));
+        scale = Math.max(0.4, Math.min(scale, 3));
 
         const baseUnvisitedRadius = 4 * scale;
         const baseVisitedRadius = 5 * scale;
